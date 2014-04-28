8718fefe6aa0293e65987e5b6a28e0df177f7b38
Boska <mr.boska@gmail.com>
2014-04-28 22:47:58 +0800

    email stmp sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,15 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.action_mailer.default_url_options = { :host => 'iosjobs.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
