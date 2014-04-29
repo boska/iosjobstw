abc29a8b4a83e910735b4c11dedc8858c9c1340f
Boska <boska@roamandwander.com>
2014-04-29 15:53:38 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-h = {"job_title"=>"Rails/iOS/Android/Art 夥伴", "company_name"=>"RRR", "url"=>"http://google.com", "job_type"=>"1", "location"=>"Remote", "description"=>"TEST", "apply"=>"TEST"}
-50.times do
-  Job.create(h)
-end
+
+
+# h = {"job_title"=>"Rails/iOS/Android/Art 夥伴", "company_name"=>"RRR", "url"=>"http://google.com", "job_type"=>"1", "location"=>"Remote", "description"=>"TEST", "apply"=>"TEST"}
+# 50.times do
+#   Job.create(h)
+# end
