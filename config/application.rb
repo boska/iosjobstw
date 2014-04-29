46026fb9c35e4d10db8bd9bf70d7d0650706cd5a
Boska <boska@roamandwander.com>
2014-04-29 16:07:30 +0800

    i18n heroku bug

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Iosjobstw
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale =  "zh-TW"
     config.i18n.locale = "zh-TW"
+    config.i18n.enforce_available_locales = false
   end
 end
