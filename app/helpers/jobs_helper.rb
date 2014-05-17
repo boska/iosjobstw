f64264f6e230dacfe5a865d774d86821ac4022d2
Boska <boska@roamandwander.com>
2014-05-17 14:08:18 +0800

    jobs layout

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,5 +1,8 @@
 module JobsHelper
-	def render_format_for(body)
-		auto_link(simple_format(body))
-	end
+  def render_format_for(body)
+    auto_link(simple_format(body))
+  end
+  def render_brief(body)
+    truncate(body,length: 100)
+  end
 end
